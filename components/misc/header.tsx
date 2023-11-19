36209b329c4f3c827dba4e2ed4a1ff116832dcc0
nedzen <nedzen.11@gmail.com>
2023-11-20 00:33:54 +0100

    test

--- components/misc/header.tsx
+++ components/misc/header.tsx
@@ -19,9 +19,7 @@ const Header = () => {
       <div className="max-w-6xl mx-auto px-5 sm:px-6">
         <div className="flex items-center justify-between h-16 md:h-20">
           <h2 className="shrink-0 mr-4 text-2xl font-bold tracking-tight md:tracking-tighter leading-tight">
-            <Link href="/" className="block hover:underline" aria-label="My Blog">
-              My Blog.
-            </Link>
+            <Link href="/" className="block hover:underline" aria-label="Marius was here"> Marius was here </Link>
           </h2>
           <ul className="flex grow justify-end flex-wrap items-center">
             <li>
