e8bb9a8abd9c37c66980eb26c8eb5f0cb0f6510f
nedzen <nedzen.11@gmail.com>
2024-02-12 16:49:38 +0100

    buggy commit

--- components/blog/post-meta.tsx
+++ components/blog/post-meta.tsx
@@ -13,23 +13,12 @@ const PostMeta = ({
   if (!(author || date)) return null;
   return (
     <>
-      <div className="flex items-center">
-        {author && (
-          <div className="flex shrink-0 mr-3">
-            <a className="relative" href="#0">
-              <span className="absolute inset-0 -m-px" aria-hidden="true"><span className="absolute inset-0 -m-px bg-white rounded-full"></span></span>
-              <img className="relative rounded-full" src={author.picture} width="32" height="32" alt="Author" />
-            </a>
-          </div>
-        )}
+      <div className="meta">
         <div>
-          {author && (
-            <>
-              <strong>{author.name}</strong>
-            </>
-          )}
+          {author && (<strong>{author.name}</strong>)}
           {(author && date) && <span> whatever </span>}
           {date && (<span><DateFormatter dateString={date} /></span>)}
+          {author.picture}
         </div>
       </div>
     </>
